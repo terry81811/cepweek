a322fe5869676e7cbedaf5cac5616b0551299bdd
Terry Tsai <terrytsai0811@gmail.com>
2014-04-26 18:56:37 +0800

    credit card

---
+++ application/views/cep/test_credit.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>信用卡付款測試頁</title>
+</head>
+<body>
+
+		<?php 
+			echo $MID."<br>";
+			echo $CID."<br>";
+			echo $TID."<br>";
+			echo $ONO."<br>";
+			echo $TA."<br>";
+			echo $U."<br>";
+			echo $M."<br>";
+
+		?>
+	<form action="https://acqtest.esunbank.com.tw/acq_online/online/sale42.htm" method="post">
+		
+
+
+		<input type="hidden" name="MID" value="<? echo $MID;?>">
+		<input type="hidden" name="CID" value="<? echo $CID;?>">
+		<input type="hidden" name="TID" value="<? echo $TID;?>">
+		<input type="hidden" name="ONO" value="<? echo $ONO;?>">
+		<input type="hidden" name="TA" value="<? echo $TA;?>">
+		<input type="hidden" name="U" value="<? echo $U;?>">
+		<input type="hidden" name="M" value="<? echo $M;?>">
+
+		<input type="submit" name="bttSubmit" value="送出">
+	</form>
+
+</body>
+</html>
