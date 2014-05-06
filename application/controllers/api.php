bd0bc40722e7c9f9285ac0f2a227a99a54472695
Terry Tsai <terrytsai0811@gmail.com>
2014-05-06 10:55:14 +0800

    internal update api

--- application/controllers/api.php
+++ application/controllers/api.php
@@ -753,10 +753,11 @@ public function webATM_return()
 
     public function update()
     {
-        $orders = $this->order_model->get(array('order_success' => '1'))
+        $orders = $this->order_model->get(array('order_success' => '1'));
         foreach ($orders as $_key => $_order) {
             $order_id = $_order['order_id'];
             $this->receive_model->update(['rec_pay_success' => '1'], ['rec_order_id' => $order_id]);
+            echo $order_id."<BR>";
         }
 
     }
