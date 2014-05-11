4d84968b218594ac70625c6f1b763765ae3ea953
kpman <s92f002@hotmail.com>
2014-05-11 11:21:16 +0800

    modify orderjs

--- application/views/cep/orderjs.php
+++ application/views/cep/orderjs.php
@@ -1,6 +1,3 @@
 <script src="<?php echo base_url(); ?>assets/js/lib/jquery.validate.min.js"></script>
 <script src="<?php echo base_url(); ?>assets/js/lib/alertify.min.js"></script>
 <script src="<?php echo base_url(); ?>assets/js/order.js"></script>
-<script>
-    // $("#order-form-validator").validate();
-</script>
